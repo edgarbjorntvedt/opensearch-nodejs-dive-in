c9dace11a0ea3bf8bb691029f251d4fd174f2d4c
Edgar Bjørntvedt <eb@email.24sevenoffice.com>
2022-12-04 21:40:37 +0100

    my config

--- config.js
+++ config.js
@@ -1,18 +1,52 @@
 require("dotenv").config();
-const { Client } = require("@opensearch-project/opensearch");
 
-// full_format_recipes.json taken from
-// https://www.kaggle.com/hugodarwood/epirecipes?select=full_format_recipes.json
-module.exports.recipes = require("./full_format_recipes.json");
+const { Client, Connection } = require("@opensearch-project/opensearch");
+const { defaultProvider } = require("@aws-sdk/credential-provider-node");
+const aws4 = require("aws4");
 
+module.exports.indexName = process.env.DATA_FILE.replace('.json', '');
+
+// kunde.140.json taken from
+// https://drive.google.com/file/d/1sN1-pvw2knc4uadq4kPjH1V60UpnYbdX/view
+module.exports.data = require("./"+process.env.DATA_FILE);
 
 /**
  * Client performs requests on our behalf
- * Additionally, when creating a client you can also specify `ssl configuration`, `bearer token`, `CA fingerprint` and other authentication details depending on protocols you use.
+ * Additionally, when creating a client you can also specify `ssl configuration`,
+ * `bearer token`, `CA fingerprint` and other authentication details depending on protocols you use.
  */
 module.exports.client = new Client({
     node: process.env.SERVICE_URI,
 });
 
-module.exports.indexName = "recipes";
 
+const createAwsConnector = (credentials, region) => {
+    class AmazonConnection extends Connection {
+        buildRequestObject(params) {
+            const request = super.buildRequestObject(params);
+            request.service = 'es';
+            request.region = region;
+            request.headers = request.headers || { Authorization: "Basic adminUser 9677dd8e-e784-c1b6-3748-a2425f3d5ceb@Q"};
+            request.headers['host'] = request.hostname;
+
+            return aws4.sign(request, credentials);
+        }
+    }
+    return {
+        Connection: AmazonConnection
+    };
+};
+
+module.exports.getClient = async () => {
+    const credentials = await defaultProvider()();
+    return new Client({
+        ...createAwsConnector(credentials, process.env.AWS_REGION),
+        // auth:{
+        //     username: 'adminUser',
+        //     password: '9677dd8e-e784-c1b6-3748-a2425f3d5ceb@Q'
+        // },
+        node: process.env.SERVICE_URI,
+        //node: host,
+    });
+}
+// console.log(process.env.SERVICE_URI)
--- index.js
+++ index.js
@@ -1,55 +1,125 @@
-const { client, indexName, recipes } = require("./config");
-const { logBody } = require("./helpers");
+const {client, indexName, data, getClient} = require("./config");
+const {logBody} = require("./helpers");
+var chunk = require('lodash.chunk');
+
+function getChunks(groups) {
+    return chunk(groups, 500)
+}
+
+/**
+ * Print a summary and view the first group
+ * Format: action \n document \n action \n document ...
+ * run-func index.js viewData
+ */
+module.exports.viewData = () => {
+    const groups = Object.values(data.groups)
+    const chunks = getChunks(groups)
+    console.log('First group: ', JSON.stringify(groups[0], null, 4));
+    console.log(`All data: ${groups.length} groups`);
+    console.log(`${chunks.length} chunks`);
+    console.log("first chunk is", JSON.stringify(chunks[0]).length, `bytes long`);
+    console.log(`${chunks[0].length} chunk length`);
+    console.log(`indexName: ${indexName}`);
+};
+/**
+ * Testing async function
+ * Format: action \n document \n action \n document ...
+ * run-func index.js runAsync
+ */
+module.exports.runAsync = async () => {
+    console.log('runAsync start');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log('runAsync finished');
+};
+/**
+ * Indexing data from json file with recipes.
+ * Format: action \n document \n action \n document ...
+ * run-func index.js injectData
+ */
+module.exports.injectData = () => {
+    const groups = Object.values(data.groups)
+    console.log(`Ingesting data: ${groups.length} groups`);
+    const body = groups.map(doc => [
+        {index: {_index: indexName}},
+        doc,
+    ]);
+
+    client.bulk({refresh: true, body}, logBody);
+};
 
 /**
  * Indexing data from json file with recipes.
  * Format: action \n document \n action \n document ...
- * run-func index injectData
+ * run-func index.js injectDataChunks
  */
-module.exports.injectData = () => {
-  console.log(`Ingesting data: ${recipes.length} recipes`);
-  const body = recipes.flatMap(doc => [
-    { index: { _index: indexName } },
-    doc,
-  ]);
-
-  client.bulk({ refresh: true, body }, logBody);
+module.exports.injectDataChunks = async () => {
+    const groups = Object.values(data.groups)
+    const chunks = getChunks(groups)
+    const client = await getClient()
+    // injest data in chunks
+    chunks.forEach((chunk, index) => {
+        console.log(`Ingesting data: ${chunk.length} groups`);
+        const body = chunk.map(doc => [
+            {index: {_index: indexName}},
+            doc,
+        ]);
+        client.bulk({refresh: true, body}, logBody);
+    })
+};
+/**
+ * run-func index.js injectDataChunk_1
+ */
+module.exports.injectDataChunk_1 = async () => {
+    const groups = Object.values(data.groups)
+    const chunks = getChunks(groups)
+    const client = await getClient()
+    const chunk = chunks[0]
+    // injest data
+    console.log(`Ingesting data: ${chunk.length} groups`);
+    const body = chunk.map(doc => [
+        {index: {_index: indexName}},
+        doc,
+    ]);
+    client.bulk({refresh: true, body}, logBody);
 };
 
 /**
  * Getting list of indices
- * run-func index getIndices
+ * run-func index.js getIndices
  */
-module.exports.getIndices = () => {
-  console.log(`Getting existing indices:`);
-  client.cat.indices({ format: "json" }, logBody);
+module.exports.getIndices = async () => {
+    console.log(`Getting existing indices:`);
+    const client = await getClient()
+
+    client.cat.indices({format: "json"}, logBody);
 };
 
 /**
  * Retrieving mapping for the index.
- * run-func index getMapping
+ * run-func index.js getMapping
  */
-module.exports.getMapping = () => {
-  console.log(`Retrieving mapping for the index with name ${indexName}`);
+module.exports.getMapping = async () => {
+    const client = await getClient()
+    console.log(`Retrieving mapping for the index with name ${indexName}`);
 
-  client.indices.getMapping({ index: indexName }, (error, result) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(result.body.recipes.mappings.properties);
-    }
-  });
+    client.indices.getMapping({index: indexName}, (error, result) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(result.body.recipes.mappings.properties);
+        }
+    });
 };
 
 /**
  * Deleting the index
- * run-func index delete
+ * run-func index.js delete
  */
 module.exports.delete = (index) => {
-  client.indices.delete(
-      {
-        index: index || indexName,
-      },
-      logBody
-  );
+    client.indices.delete(
+        {
+            index: index || indexName,
+        },
+        logBody
+    );
 };
